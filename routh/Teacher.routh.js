591f0c62b36f8db5e28cd4eebaeac938a6ff8176
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-24 15:45:43 +0530

    routh

--- routh/Teacher.routh.js
+++ routh/Teacher.routh.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { uploadFile } from "../controller/teacher.classes.controller.js";
-import multer from 'multer';
-const Upload = multer({ dest: 'uploads/' })
+
+import multer from "multer";
+import { uploadLecture } from "../controller/teacher.classes.controller.js";
+import { verifyToken } from "../middleware/Verifytoken.js";
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage })
 const teacherRouter = express.Router();
 
-teacherRouter.post("/upload", Upload.single('image') , uploadFile);
-
-
-
+teacherRouter.post("/upload-lecture", upload.single("file"),verifyToken, uploadLecture);
 export default teacherRouter;
