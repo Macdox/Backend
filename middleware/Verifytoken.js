1843e7d10172d3beaeb49afd2623754d32e352ef
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-29 21:39:58 +0530

    changes

--- middleware/Verifytoken.js
+++ middleware/Verifytoken.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
+  console.log("Cookies:", req.cookies);
+  console.log("Authorization Header:", req.headers.authorization);
+
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(403).json({ message: "No token provided" });
