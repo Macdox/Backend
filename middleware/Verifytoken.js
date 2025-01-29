89eecf267c86506af07187b9e5a80be051768c4c
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-29 21:01:57 +0530

    pass

--- middleware/Verifytoken.js
+++ middleware/Verifytoken.js
@@ -1,23 +1,19 @@
 import jwt from "jsonwebtoken";
-import express from "express";
-import cookieParser from "cookie-parser";
-
-const app = express();
-
-app.use(express.json());
-app.use(cookieParser());
 
 export const verifyToken = (req, res, next) => {
-  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
-  if (!token) {
-    return res.status(403).json({ message: "No token provided" });
-  }
+	console.log(req.cookies);
+	const token = req.cookies.token;
+	console.log("token:", req.cookies);
+	if (!token) return res.status(401).json({ success: false, message: "Unauthorized - no token provided" });
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decoded.teacherId || decoded.studentId;
-    next();
-  } catch (error) {
-    return res.status(401).json({ message: "Unauthorized" });
-  }
+		if (!decoded) return res.status(401).json({ success: false, message: "Unauthorized - invalid token" });
+		req.userId = decoded.teacherId;
+		//console.log("decoded-teacher:", req.userId);	
+		next();
+	} catch (error) {
+		//console.log("Error in verifyToken ", error);
+		return res.status(500).json({ success: false, message: "Server error" });
+	}
 };
