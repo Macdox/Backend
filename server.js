d41aa4ad1e557010695ae461cb494279fdefb08b
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-29 14:38:35 +0530

    app

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://spiroedu.netlify.app", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
