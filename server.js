03671148993d196244be959bcbdb6fede88078fc
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-30 18:36:13 +0530

    save

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: "https://spiroedu.netlify.app",
+  origin: " http://localhost:5173",
   // " // Your frontend domain
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
