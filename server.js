df26c5a6306ef9bf1831bec7812ac44fbff1d83d
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-30 22:40:06 +0530

    setup

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: process.env.URL,
   // Your frontend domain
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"]
