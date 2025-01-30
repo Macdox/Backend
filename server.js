a46b9498b1ca3349e2620b958ffddc49f7dac3c8
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-30 16:48:39 +0530

    add

--- server.js
+++ server.js
@@ -22,14 +22,19 @@ app.use(cors({
 app.use(cookieParser());
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('combined')); // Use Morgan for logging in production
 
 app.use('/api/v1', router);
 app.use('/api/v1', teacherRouter);
 app.use('/api/v1', Studentrouter);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
     connectDB();
