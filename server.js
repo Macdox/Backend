0d6801454827f04ae7c7beac19b02e61f88d312e
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-01-30 17:39:29 +0530

    post

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
   origin: ["*"], // Your frontend domain
-  credentials: true
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 
