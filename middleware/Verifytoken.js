edef26bae1c6b71ba2b4f2193466700ccca6f1cb
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-02-05 18:16:06 +0530

    push

--- middleware/Verifytoken.js
+++ middleware/Verifytoken.js
@@ -2,11 +2,10 @@ import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
     let token = req.headers?.Authorization; // Get token from header
+    console.log("Token from header: ", token);
     if (!token) {
         token = req.session?.token; // Get token from sessionStorage
-    }
-    if (!token) {
-        token = req.cookies?.token; // Get token from cookie
+        console.log("Token from session: ", token);
     }
     if (!token) {
         return res.status(401).json({ message: "Unauthorized: No token provided" });
@@ -14,6 +13,7 @@ const verifyToken = (req, res, next) => {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.userId; // Attach user info to request
+        console.log("Decoded: ", decoded);
         next();
     } catch (error) {
         return res.status(403).json({ message: "Invalid or expired token" });
