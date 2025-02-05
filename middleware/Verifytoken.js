5e4d9c8240166c89c13d45c3841a5a31cea8c209
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-02-05 18:16:46 +0530

    push2

--- middleware/Verifytoken.js
+++ middleware/Verifytoken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-    let token = req.headers?.Authorization; // Get token from header
+    let token = req.headers.authorization?.split(" ")[1]; // Get token from header
     console.log("Token from header: ", token);
     if (!token) {
         token = req.session?.token; // Get token from sessionStorage
@@ -10,6 +10,7 @@ const verifyToken = (req, res, next) => {
     if (!token) {
         return res.status(401).json({ message: "Unauthorized: No token provided" });
     }
+
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.userId; // Attach user info to request
