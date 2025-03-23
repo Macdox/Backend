f27847bcc2ec8996a2b677345d5747d0929617b1
Tejas Najare <145053380+Macdox@users.noreply.github.com>
2025-03-24 00:52:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use('/api/v1', Studentrouter);
 
 
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   connectDB();
   api();
   console.log(`Server is running on port ${PORT}`);
